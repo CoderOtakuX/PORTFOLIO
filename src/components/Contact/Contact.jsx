74f93d8da2b85b0cda54c2e281664bc0bb8725d3
Arjun_Negi <156993385+CoderOtakuX@users.noreply.github.com>
2024-03-28 10:40:22 +0530

    main
    main uploaded

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:arjun432an2@gmail.com">arjun432an2@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/arjunnegi" target="_blank">www.linkedin.com/arjunnegi</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/CoderOtakuX" target="_blank">github.com/CoderOtakuX</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
